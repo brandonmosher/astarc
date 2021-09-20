b4a160e98e763985a9e8351dfffe1593e3a43a3c
Brandon Mosher <code.brandon@mosher.xyz>
2021-09-20 12:25:23 -0400

    Updated install script

--- install_pyctemp.sh
+++ install_pyctemp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/mosherbd/pyctemp.git
+git clone https://github.com/brandonmosher/pyctemp.git
 chmod +x pyctemp/install.sh
 ./pyctemp/install.sh
 rm -rf pyctemp
